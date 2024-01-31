68f4418a26c2bdab164c75daf00ce44c390a1a44
TojiboyevAzizbek <157882322+TojiboyevAzizbek@users.noreply.github.com>
2024-01-31 23:54:17 +0500

    uyga vazifa yuklandi
    uyga vazifa yuklandi

---
+++ vazifa.py
@@ -0,0 +1,61 @@
+# Masala 1
+
+# def check_in(a,b):
+#     if a in b:
+#         return True
+#     else:
+#         return False
+
+# a = input('biror nima kiriting ')
+# b = input('biror nima kiriting ')
+# result = check_in(a,b)
+# print(result)
+
+
+# Masala 2
+
+# def check_unli(matn)->str:
+#     unli = 'AOUIEaouie'
+#     count = 0
+#     for i in matn:
+#         if i in unli:
+#             count+=1
+#     return count
+            
+# matn = input("Matn kiriting ")
+# result = check_unli(matn)
+# print(result)
+
+
+
+
+#  Masala 3
+
+# def check_zero(numbers):
+#     num = '0'
+#     count = 0
+#     for i in numbers: 
+#         if i in num:
+#             count+=1
+#     return count
+# numbers = input('Raqamlar kiriting ')
+# result = check_zero(numbers)
+# print(result)
+
+
+
+
+# Masala  4
+
+# def check_harflar(a, b):
+#     unli = "AOUIEaouie"
+#     unlilar = set(i for i in a if i in unli)
+#     return all(i in unlilar for i in b)
+
+# a = input("Ma'lumot kiriting ")
+# b = input("Ma'lumot kiriting ")
+# print(check_harflar(a, b))
+
+
+
+
